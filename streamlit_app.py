79762298880c8d8d9b6872bc32c0d3f018a586ac
RyuShige <66407509+RyuShige@users.noreply.github.com>
2024-09-24 22:03:28 +0900

    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -95,9 +95,10 @@ st.title('映画推薦システム')
 user_movies = st.multiselect('好きな映画を選択してください', movie_title)
 
 # ユーザーが希望する特徴を入力
-query = st.text_input('見たい映画の特徴やジャンルを入力してください')
+query = st.text_input('今みたい映画の特徴やジャンルを入力してください')
 
 if st.button('おすすめの映画を表示'):
+    st.write('**以下があなたが好きな映画と今みたい気分にあわせたレコメンドです**', )
     if user_movies and query:
         # ユーザーが選択した映画のIDを取得
         user_movie_ids = [np.where(movie_title == movie)[0][0] for movie in user_movies]
